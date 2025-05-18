893e6a9b80755d227f12df4fbb6c2124992f5bdf
numshv <noumisyifa@gmail.com>
2025-05-18 10:11:43 +0700

    test: test inputFileHandler

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,9 @@
+import utils.*;
+import obj.*;
+
 public class Main {
-    
+    public static void main(String[] args) {
+        Utils utils = new Utils();
+        Board board = utils.inputFileHandler();
+    }
 }
